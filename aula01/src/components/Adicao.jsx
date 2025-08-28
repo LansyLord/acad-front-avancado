410a9b7c01a9de3ff38f169567367ac8c3d1814d
lansylord <lansylord@gmail.com>
2025-08-27 21:05:43 -0300

    Primeiro commit aula0 e aula01

---
+++ aula01/src/components/Adicao.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Adicao = (props) => {
+
+
+    return (
+        <div>O resultado de {props.num1} + {props.num2} é {props.num1 + props.num2}</div>
+    )
+}
+
+export default Adicao
